25bfff93ca84350f4312eab7229b3204401a41a0
David <grigorenkodavid@gmail.com>
2023-03-26 01:00:32 +0300

    [GOHW-5]: Added test for Purchase business logic.

---
+++ checkout/internal/domain/purchase_test.go
@@ -0,0 +1,149 @@
+package domain
+
+import (
+	"context"
+	"errors"
+	lomsClientMock "route256/checkout/internal/clients/mocks/loms"
+	productClientMock "route256/checkout/internal/clients/mocks/product"
+	"route256/checkout/internal/models"
+	cartsRepoMock "route256/checkout/internal/repository/mock/carts"
+	trMock "route256/libs/transactor/mock"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gojuno/minimock/v3"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPurchase(t *testing.T) {
+	ctx := context.Background()
+
+	userIDMock := models.User(gofakeit.Int64())
+	cartItemsMock := make([]models.CartItem, 10)
+	gofakeit.Slice(&cartItemsMock)
+	orderIDMock := new(models.OrderID)
+	*orderIDMock = models.OrderID(gofakeit.Int64())
+
+	defaultTrMock := func(mc *minimock.Controller) *trMock.TransactionRunnerMock {
+		return trMock.NewTransactionRunnerMock(mc).RunSerializableMock.Set(
+			func(ctx context.Context, txFn func(ctx context.Context) error) (err error) {
+				return txFn(ctx)
+			},
+		)
+	}
+	defaultLomsClientMock := func(mc *minimock.Controller) *lomsClientMock.LOMSServiceClientMock {
+		lomsClient := lomsClientMock.NewLOMSServiceClientMock(mc)
+		return lomsClient.CreateOrderMock.Return(orderIDMock, nil)
+	}
+	defaultCartRepoMock := func(mc *minimock.Controller) *cartsRepoMock.CartsRepositoryMock {
+		cartsRepo := cartsRepoMock.NewCartsRepositoryMock(mc)
+		cartsRepo = cartsRepo.GetCartItemsMock.Return(cartItemsMock, nil)
+		i := 0
+		cartsRepo = cartsRepo.DeleteCartItemMock.Inspect(func(ctx context.Context, user models.User, sku models.SKU) {
+			require.Equal(mc, cartItemsMock[i].SKU, sku)
+			i++
+		}).Return(nil)
+		return cartsRepo
+	}
+
+	var (
+		cartsRepoError   = errors.New("Some cart item repo error")
+		transactionError = errors.New("Some transaction error")
+	)
+
+	tests := []struct {
+		Name                             string
+		ExpectedResult                   *models.OrderID
+		ExpectedErr                      error
+		Tr                               func(mc *minimock.Controller) *trMock.TransactionRunnerMock
+		LOMSClient                       func(mc *minimock.Controller) *lomsClientMock.LOMSServiceClientMock
+		CartsRepo                        func(mc *minimock.Controller) *cartsRepoMock.CartsRepositoryMock
+		ExpectedDeleteCartItemCallsCount uint64
+	}{
+		{
+			Name:                             "success",
+			ExpectedResult:                   orderIDMock,
+			ExpectedErr:                      nil,
+			Tr:                               defaultTrMock,
+			LOMSClient:                       defaultLomsClientMock,
+			CartsRepo:                        defaultCartRepoMock,
+			ExpectedDeleteCartItemCallsCount: uint64(len(cartItemsMock)),
+		},
+		{
+			Name:           "get cart item repo error",
+			ExpectedResult: orderIDMock,
+			ExpectedErr:    cartsRepoError,
+			Tr:             defaultTrMock,
+			LOMSClient:     defaultLomsClientMock,
+			CartsRepo: func(mc *minimock.Controller) *cartsRepoMock.CartsRepositoryMock {
+				cartsRepo := cartsRepoMock.NewCartsRepositoryMock(mc)
+				cartsRepo = cartsRepo.GetCartItemsMock.Return(nil, cartsRepoError)
+				return cartsRepo
+			},
+			ExpectedDeleteCartItemCallsCount: 0,
+		},
+		{
+			Name:           "order creation error",
+			ExpectedResult: orderIDMock,
+			ExpectedErr:    OrderCreationError,
+			Tr:             defaultTrMock,
+			LOMSClient: func(mc *minimock.Controller) *lomsClientMock.LOMSServiceClientMock {
+				lomsClient := lomsClientMock.NewLOMSServiceClientMock(mc)
+				lomsClient.CreateOrderMock.Return(nil, OrderCreationError)
+				return lomsClient
+			},
+			CartsRepo:                        defaultCartRepoMock,
+			ExpectedDeleteCartItemCallsCount: 0,
+		},
+		{
+			Name:           "delete cart item repo error",
+			ExpectedResult: orderIDMock,
+			ExpectedErr:    cartsRepoError,
+			Tr:             defaultTrMock,
+			LOMSClient:     defaultLomsClientMock,
+			CartsRepo: func(mc *minimock.Controller) *cartsRepoMock.CartsRepositoryMock {
+				cartsRepo := cartsRepoMock.NewCartsRepositoryMock(mc)
+				cartsRepo = cartsRepo.GetCartItemsMock.Return(cartItemsMock, nil)
+				cartsRepo = cartsRepo.DeleteCartItemMock.Return(cartsRepoError)
+				return cartsRepo
+			},
+			ExpectedDeleteCartItemCallsCount: 1,
+		},
+		{
+			Name:           "transaction error",
+			ExpectedResult: orderIDMock,
+			ExpectedErr:    transactionError,
+			Tr: func(mc *minimock.Controller) *trMock.TransactionRunnerMock {
+				tr := trMock.NewTransactionRunnerMock(mc)
+				tr = tr.RunSerializableMock.Inspect(func(ctx context.Context, txFn func(ctx context.Context) error) {
+					err := txFn(ctx)
+					require.Equal(mc, nil, err)
+				}).Return(transactionError)
+				return tr
+			},
+			LOMSClient:                       defaultLomsClientMock,
+			CartsRepo:                        defaultCartRepoMock,
+			ExpectedDeleteCartItemCallsCount: uint64(len(cartItemsMock)),
+		},
+	}
+	for _, testCase := range tests {
+		t.Run(testCase.Name, func(t *testing.T) {
+			mc := minimock.NewController(t)
+			cartsRepo := testCase.CartsRepo(mc)
+			s := New(
+				testCase.Tr(mc),
+				cartsRepo,
+				testCase.LOMSClient(mc),
+				productClientMock.NewProductServiceClientMock(mc),
+			)
+			orderID, err := s.MakePurchase(ctx, userIDMock)
+			if testCase.ExpectedErr != nil {
+				require.ErrorIs(t, err, testCase.ExpectedErr)
+			} else {
+				require.Equal(t, *orderID, *testCase.ExpectedResult)
+				require.Equal(t, err, nil)
+			}
+			require.Equal(t, testCase.ExpectedDeleteCartItemCallsCount, cartsRepo.DeleteCartItemAfterCounter())
+		})
+	}
+}
